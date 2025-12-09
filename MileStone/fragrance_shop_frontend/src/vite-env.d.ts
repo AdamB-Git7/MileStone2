b7f564af8c8b88f38edcfbb6c14d8423673ed61b
Adam <benikhlef.adam@gmail.com>
2025-12-08 20:41:27 -0500

    Add vite and node type declarations

---
+++ MileStone/fragrance_shop_frontend/src/vite-env.d.ts
@@ -0,0 +1,10 @@
+﻿/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_API_URL?: string;
+  readonly REACT_APP_API_URL?: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
